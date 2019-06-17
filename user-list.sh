5eb34a0888adb086f62fa95474003e1fbe9d2471
kambuse <41556468+kambuse@users.noreply.github.com>
2019-06-17 15:16:20 +0800

    Script

--- user-list.sh
+++ user-list.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo "-------------------------------"
-echo "USERNAME          EXP DATE     "
-echo "-------------------------------"
+echo "-------------------------------" | lolcat
+echo "USERNAME          EXP DATE     " | lolcat
+echo "-------------------------------" | lolcat
 while read expired
 do
         AKUN="$(echo $expired | cut -d: -f1)"
         ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
         exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
         if [[ $ID -ge 1000 ]]; then
-        printf "%-17s %2s\n" "$AKUN" "$exp"
+        printf "%-17s %2s\n" "$AKUN" "$exp" | lolcat
         fi
 done < /etc/passwd
 JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
-echo "-------------------------------"
-echo "Jumlah akun: $JUMLAH user"
-echo "-------------------------------"
-echo -e "~sivoi~"
+echo "-------------------------------" | lolcat
+echo "Jumlah akun: $JUMLAH user" | lolcat
+echo "-------------------------------" | lolcat
+echo -e "Script By Nazril Purnomo" | lolcat
