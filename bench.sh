a912455460e54360f237af6aa905578d0dcb2d14
kambuse <41556468+kambuse@users.noreply.github.com>
2019-06-17 18:40:06 +0800

    Script

--- bench.sh
+++ bench.sh
@@ -15,27 +15,24 @@ echo "Total amount of swap : $swap MB" | lolcat
 echo "System uptime : $up" | lolcat
 
 cachefly=$( wget -O /dev/null http://cachefly.cachefly.net/100mb.test 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-echo "Download speed from CacheFly: $cachefly "
+echo "Download speed from CacheFly: $cachefly " | lolcat
 coloatatl=$( wget -O /dev/null http://speed.atl.coloat.com/100mb.test 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-echo "Download speed from Coloat, Atlanta GA: $coloatatl "
+echo "Download speed from Coloat, Atlanta GA: $coloatatl " | lolcat
 sldltx=$( wget -O /dev/null http://speedtest.dal05.softlayer.com/downloads/test100.zip 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-echo "Download speed from Softlayer, Dallas, TX: $sldltx "
+echo "Download speed from Softlayer, Dallas, TX: $sldltx " | lolcat
 linodejp=$( wget -O /dev/null http://speedtest.tokyo.linode.com/100MB-tokyo.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-echo "Download speed from Linode, Tokyo, JP: $linodejp "
+echo "Download speed from Linode, Tokyo, JP: $linodejp " | lolcat
 i3d=$( wget -O /dev/null http://mirror.i3d.net/100mb.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-echo "Download speed from i3d.net, Rotterdam, NL: $i3d"
-
-#linodeuk=$( wget -O /dev/null http://speedtest.london.linode.com/100MB-london.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-#echo "Download speed from Linode, London, UK: $linodeuk "
+echo "Download speed from i3d.net, Rotterdam, NL: $i3d" | lolcat
 leaseweb=$( wget -O /dev/null http://mirror.leaseweb.com/speedtest/100mb.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-echo "Download speed from Leaseweb, Haarlem, NL: $leaseweb "
+echo "Download speed from Leaseweb, Haarlem, NL: $leaseweb " | lolcat
 slsg=$( wget -O /dev/null http://speedtest.sng01.softlayer.com/downloads/test100.zip 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-echo "Download speed from Softlayer, Singapore: $slsg "
+echo "Download speed from Softlayer, Singapore: $slsg " | lolcat
 slwa=$( wget -O /dev/null http://speedtest.sea01.softlayer.com/downloads/test100.zip 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-echo "Download speed from Softlayer, Seattle, WA: $slwa "
+echo "Download speed from Softlayer, Seattle, WA: $slwa " | lolcat
 slsjc=$( wget -O /dev/null http://speedtest.sjc01.softlayer.com/downloads/test100.zip 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-echo "Download speed from Softlayer, San Jose, CA: $slsjc "
+echo "Download speed from Softlayer, San Jose, CA: $slsjc " | lolcat
 slwdc=$( wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test100.zip 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-echo "Download speed from Softlayer, Washington, DC: $slwdc "
+echo "Download speed from Softlayer, Washington, DC: $slwdc " | lolcat
 io=$( ( dd if=/dev/zero of=test_$$ bs=64k count=16k conv=fdatasync && rm -f test_$$ ) 2>&1 | awk -F, '{io=$NF} END { print io}' )
-echo "I/O speed : $io"
+echo "I/O speed : $io" | lolcat
