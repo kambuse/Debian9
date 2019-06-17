bbad31599e28a31988cde3126d6956853d416725
kambuse <41556468+kambuse@users.noreply.github.com>
2019-06-17 14:52:31 +0800

    Script

--- trial.sh
+++ trial.sh
@@ -9,13 +9,13 @@ Pass=`</dev/urandom tr -dc a-f0-9 | head -c9`
 useradd -e `date -d "$hari days" +"%Y-%m-%d"` -s /bin/false -M $Login
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "====Trial SSH Account===="
-echo -e "Host           : $IP" 
-echo -e "Port OpenSSH   : 22,143"
-echo -e "Port Dropbear  : 80,444"
-echo -e "Port SSL/TLS   : 443"
-echo -e "Port Squid     : 8080,3128"
-echo -e "Config OpenVPN : http://$IP:81/client.ovpn"
-echo -e "Username       : $Login"
-echo -e "Password       : $Pass\n"
-echo -e "========================="
+echo -e "====Trial SSH Account====" | lolcat
+echo -e "Host           : $IP"  | lolcat
+echo -e "Port OpenSSH   : 22" | lolcat
+echo -e "Port Dropbear  : 80,109, 110" | lolcat
+echo -e "Port SSL/TLS   : 443" | lolcat
+echo -e "Port Squid     : 8080,8000" | lolcat
+echo -e "Config OpenVPN : http://$IP:81/client.ovpn" | lolcat
+echo -e "Username       : $Login" | lolcat
+echo -e "Password       : $Pass\n" | lolcat
+echo -e "=========================" | lolcat
