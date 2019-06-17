0d260de6c59c672da0db237d6851724903d8ef3a
kambuse <41556468+kambuse@users.noreply.github.com>
2019-06-17 18:31:24 +0800

    Script

--- bench.sh
+++ bench.sh
@@ -8,11 +8,11 @@ swap=$( free -m | awk 'NR==4 {print $2}' )
 up=$(uptime|awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
 
 echo "CPU model : $cname" | lolcat
-echo "Number of cores : $cores"
-echo "CPU frequency : $freq MHz"
-echo "Total amount of ram : $tram MB"
-echo "Total amount of swap : $swap MB"
-echo "System uptime : $up"
+echo "Number of cores : $cores" | lolcat
+echo "CPU frequency : $freq MHz" | lolcat
+echo "Total amount of ram : $tram MB" | lolcat
+echo "Total amount of swap : $swap MB" | lolcat
+echo "System uptime : $up" | lolcat
 
 cachefly=$( wget -O /dev/null http://cachefly.cachefly.net/100mb.test 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
 echo "Download speed from CacheFly: $cachefly "
