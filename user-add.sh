90fda64c5c614825392a368ef67e7e4fa3e8f3d5
kambuse <41556468+kambuse@users.noreply.github.com>
2019-06-17 14:49:52 +0800

    Script

--- user-add.sh
+++ user-add.sh
@@ -9,15 +9,15 @@ useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "====Informasi SSH Account===="
-echo -e "Host           : $IP" 
-echo -e "Port OpenSSH   : 22"
-echo -e "Port Dropbear  : 80,109, 110"
-echo -e "Port SSL/TLS   : 443"
-echo -e "Port Squid     : 8080,8000"
-echo -e "Config OpenVPN : http://$IP:85/client.ovpn"
-echo -e "Username       : $Login "
-echo -e "Password       : $Pass"
-echo -e "-----------------------------"
-echo -e "Aktif Sampai   : $exp"
-echo -e "============================="
+echo -e "====Informasi SSH Account====" | lolcat
+echo -e "Host           : $IP"  | lolcat
+echo -e "Port OpenSSH   : 22"  | lolcat
+echo -e "Port Dropbear  : 80,109, 110"  | lolcat
+echo -e "Port SSL/TLS   : 443" | lolcat
+echo -e "Port Squid     : 8080,8000"  | lolcat
+echo -e "Config OpenVPN : http://$IP:85/client.ovpn"  | lolcat
+echo -e "Username       : $Login "  | lolcat
+echo -e "Password       : $Pass"  | lolcat
+echo -e "-----------------------------"  | lolcat
+echo -e "Aktif Sampai   : $exp"  | lolcat
+echo -e "============================="  | lolcat
