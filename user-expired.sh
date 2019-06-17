8fced6dac7cb31623bccdc554f428d01dbe7f5e2
kambuse <41556468+kambuse@users.noreply.github.com>
2019-06-17 17:22:26 +0800

    Script

---
+++ user-expired.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "-------------------------------" | lolcat
+echo "DAFTAR AKUN EXPIRED" | lolcat
+echo "-------------------------------" | lolcat
+cat ~/expireduser.txt | lolcat
+echo "-------------------------------" | lolcat
+echo -e "Script By Nazril Purnomo" | lolcat
