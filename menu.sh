41d1eb428a79e7092268b803784e1ffff36f6c4f
kambuse <41556468+kambuse@users.noreply.github.com>
2019-06-18 07:12:02 +0800

    Script

--- menu.sh
+++ menu.sh
@@ -6,11 +6,15 @@ echo -e "» trial         : Membuat akun trial" | lolcat
 echo -e "» user-del      : Menghapus akun SSH & OpenVPN" | lolcat
 echo -e "» user-login    : Cek user login" | lolcat
 echo -e "» user-list     : Daftar member SSH & OpenVPN" | lolcat
+echo -e "» user-expired  : Daftar user expired" | lolcat
 echo -e "» expdel        : Delete user expired" | lolcat
 echo -e "» resvis        : Restart service dropbear, webmin" | lolcat
 echo -e "                  squid, OpenVPN,Stunnel dan SSH" | lolcat
 echo -e "» reboot        : Reboot VPS" | lolcat
 echo -e "» speedtest     : Speedtest VPS" | lolcat
+echo -e "» port-aktif    : Cek port aktif VPS" | lolcat
+echo -e "» ps-mem        : Memori Usage" | lolcat
+echo -e "» benchmark     : Benchmark" | lolcat
 echo -e "» info          : Menampilkan informasi sistem" | lolcat
 echo -e "» about         : Info script auto install" | lolcat
 echo -e "» exit          : Keluar dari terminal SSH" | lolcat
