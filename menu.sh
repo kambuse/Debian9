a182fda4a1bf2734cb9c4b9acbfe780b97d78ee5
kambuse <41556468+kambuse@users.noreply.github.com>
2019-06-17 14:34:54 +0800

    Script

--- menu.sh
+++ menu.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo -e ""
-echo -e "» menu          : Menampilkan daftar perintah"
-echo -e "» user-add      : Membuat akun SSH & OpenVPN"
-echo -e "» trial         : Membuat akun trial"
-echo -e "» user-del      : Menghapus akun SSH & OpenVPN"
-echo -e "» user-login    : Cek user login"
-echo -e "» user-list     : Daftar member SSH & OpenVPN"
-echo -e "» expdel        : Delete user expired"
-echo -e "» resvis        : Restart service dropbear, webmin"
-echo -e "                  squid, OpenVPN,Stunnel dan SSH"
-echo -e "» reboot        : Reboot VPS"
-echo -e "» speedtest     : Speedtest VPS"
-echo -e "» info          : Menampilkan informasi sistem"
-echo -e "» about         : Info script auto install"
-echo -e "» exit          : Keluar dari terminal SSH"
-echo -e "~sivoi~"
+echo -e "» menu          : Menampilkan daftar perintah" | lolcat
+echo -e "» user-add      : Membuat akun SSH & OpenVPN"  | lolcat
+echo -e "» trial         : Membuat akun trial" | lolcat
+echo -e "» user-del      : Menghapus akun SSH & OpenVPN" | lolcat
+echo -e "» user-login    : Cek user login" | lolcat
+echo -e "» user-list     : Daftar member SSH & OpenVPN" | lolcat
+echo -e "» expdel        : Delete user expired" | lolcat
+echo -e "» resvis        : Restart service dropbear, webmin" | lolcat
+echo -e "                  squid, OpenVPN,Stunnel dan SSH" | lolcat
+echo -e "» reboot        : Reboot VPS" | lolcat
+echo -e "» speedtest     : Speedtest VPS" | lolcat
+echo -e "» info          : Menampilkan informasi sistem" | lolcat
+echo -e "» about         : Info script auto install" | lolcat
+echo -e "» exit          : Keluar dari terminal SSH" | lolcat
+echo -e "Script By Nazril Purnomo" | lolcat
