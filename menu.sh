73f9306cd030aebc4fd98f64f78edea2f46f4714
kambuse <41556468+kambuse@users.noreply.github.com>
2019-06-18 19:26:34 +0800

    Script

--- menu.sh
+++ menu.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 echo -e ""
-echo -e "» menu          : Menampilkan daftar perintah" | lolcat
-echo -e "» user-add      : Membuat akun SSH & OpenVPN"  | lolcat
-echo -e "» trial         : Membuat akun trial" | lolcat
-echo -e "» user-del      : Menghapus akun SSH & OpenVPN" | lolcat
-echo -e "» user-login    : Cek user login" | lolcat
-echo -e "» user-list     : Daftar member SSH & OpenVPN" | lolcat
-echo -e "» user-expired  : Daftar user expired" | lolcat
-echo -e "» expdel        : Delete user expired" | lolcat
-echo -e "» resvis        : Restart service dropbear, webmin" | lolcat
-echo -e "                  squid, OpenVPN,Stunnel dan SSH" | lolcat
-echo -e "» reboot        : Reboot VPS" | lolcat
-echo -e "» speedtest     : Speedtest VPS" | lolcat
-echo -e "» port-aktif    : Cek port aktif VPS" | lolcat
-echo -e "» ps-mem        : Memori Usage" | lolcat
-echo -e "» benchmark     : Benchmark" | lolcat
-echo -e "» info          : Menampilkan informasi sistem" | lolcat
-echo -e "» about         : Info script auto install" | lolcat
-echo -e "» exit          : Keluar dari terminal SSH" | lolcat
+echo -e "» menu                : Menampilkan daftar perintah" | lolcat
+echo -e "» user-add            : Membuat akun SSH & OpenVPN"  | lolcat
+echo -e "» trial               : Membuat akun trial" | lolcat
+echo -e "» user-del            : Menghapus akun SSH & OpenVPN" | lolcat
+echo -e "» user-login          : Cek user login" | lolcat
+echo -e "» user-list           : Daftar member SSH & OpenVPN" | lolcat
+echo -e "» user-renew          : Tambah masa aktif akun SSH & OpenVPN" | lolcat
+echo -e "» disable-user-expire : Disable user expired" | lolcat
+echo -e "» user-expired-list   : Daftar user expired" | lolcat
+echo -e "» delete-user-expire  : Delete user expired" | lolcat
+echo -e "» resvis              : Restart service dropbear, webmin" | lolcat
+echo -e "                        squid, OpenVPN,Stunnel dan SSH" | lolcat
+echo -e "» reboot              : Reboot VPS" | lolcat
+echo -e "» speedtest           : Speedtest VPS" | lolcat
+echo -e "» port-aktif          : Cek port aktif VPS" | lolcat
+echo -e "» ps-mem              : Memori Usage" | lolcat
+echo -e "» benchmark           : Benchmark" | lolcat
+echo -e "» info                : Menampilkan informasi sistem" | lolcat
+echo -e "» about               : Info script auto install" | lolcat
+echo -e "» exit                : Keluar dari terminal SSH" | lolcat
 echo -e "Script By Nazril Purnomo" | lolcat
