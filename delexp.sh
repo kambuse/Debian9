a0f7d1b51e1c5ebd1c8794743755da6b63518fe1
kambuse <41556468+kambuse@users.noreply.github.com>
2019-06-18 17:36:59 +0800

    Script

--- delexp.sh
+++ delexp.sh
@@ -1,40 +1,25 @@
 #!/bin/bash
-
-# current time in seconds
+echo "" > /root/user.db
 datenow=$(date +%s)
-
-# loop over all users
+echo -e "=================================================" | lolcat
+printf '%40s%-10s%-5s\n' "Hapus Akun SSH/OpenVPN Expired" | lolcat
+echo -e "-------------------------------------------------" | lolcat
+printf '%-20s%-21s%-20s\n' "Username" "Expired" "Status" | lolcat
+echo -e "-------------------------------------------------" | lolcat
 for user in $(awk -F: '{print $1}' /etc/passwd); do
-
-# get date of expiration of users that have exp. date set
-expdate=$(chage -l $user|awk -F: '/Account expires/{print $2}')
-echo $expdate | grep -q never && continue
-
-echo -n "User \`$user' expired on $expdate "
-
-# convert exp. date to seconds
-expsec=$(date +%s --date="$expdate")
-
-# calculate diff b/t time now and exp. date
-diff=$( echo $datenow - $expsec)
-
-# if a negative number, hasn't expired yet
-echo $diff | grep -q ^\- && echo okay && continue
-printf ""
-
-# it will ask every user that will deleted, if you want to disable the feature you can add "#" in 3 lines
-read -r -p "Are you sure you want to delete this user? [y/N] " response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-
-# got this far, account must have expired
-echo deleting $user ...
-
-# deleting user
-userdel -r $user
-
-# if you want to disable the 'ask' feature you can add "#" in 3 lines
-else
-exit
-fi
+        expdate=$(chage -l $user|awk -F: '/Account expires/{print $2}')
+        echo $expdate|grep -q never && continue
+        datanormal=$(date -d"$expdate" '+%d/%m/%Y')
+        tput setaf 3 ; tput bold ; printf '%-20s%-21s%s' $user $datanormal ; tput sgr0
+        expsec=$(date +%s --date="$expdate")
+        diff=$(echo $datenow - $expsec|bc -l)
+        tput setaf 2 ; tput bold
+        echo $diff|grep -q ^\- && echo "Aktif" && continue
+        tput setaf 1 ; tput bold
+        echo "Hapus"
+        pkill -f $user
+        userdel $user
+        grep -v ^$user[[:space:]] /root/user.db > /tmp/ph ; cat /tmp/ph > /root/user.db
 done
+echo -e "=================================================" | lolcat
+echo -e "Script by Nazril Purnomo" | lolcat
