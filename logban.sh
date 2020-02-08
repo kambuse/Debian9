c4f628d7c3bc86166641c3d2d1ba29d5720b3da4
kambuse <41556468+kambuse@users.noreply.github.com>
2020-02-08 18:59:02 +0800

    Create logban.sh

---
+++ logban.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+            red='\e[1;31m'
+            green='\e[0;32m'
+            NC='\e[0m'
+            echo "Connecting to server..."
+            sleep 0.1
+            echo "Checking Permision..."
+            sleep 0.2
+            echo -e "${green}Permission Accepted...${NC}"
+            sleep 0.5
+            echo""
+
+            echo "Script Created by Nazril Purnomo";
+            echo ""
+            echo "===============================================";
+            echo " ";
+            if [ -e "/root/log-ban.txt" ]; then
+            echo "User yang diban oleh user-ban adalah";
+            echo "Waktu - Username - Jumlah Multilogin"
+            echo "-------------------------------------";
+            cat /root/log-ban.txt
+            else
+            echo " Tidak ada user yang melakukan pelanggaran"
+            echo " Atau"
+            echo " Script user-ban belum dijalankan"
+            fi
+            echo " ";
+            echo "===============================================";
